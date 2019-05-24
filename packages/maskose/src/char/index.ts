4c87219d8587c4d028ab24a03c6b812e86452d55
Emanuel Freitas <efreitasn@hotmail.com>
2019-05-24 20:07:34 -0300

    Move mkChar* fns to a separate folder

---
+++ packages/maskose/src/char/index.ts
@@ -0,0 +1,5 @@
+export { default as mkCharGroup } from './mkCharGroup';
+export { default as mkCharLetter } from './mkCharLetter';
+export { default as mkCharNum } from './mkCharNum';
+export { default as mkCharSpecific } from './mkCharSpecific';
+export { default as mkCharToBePut } from './mkCharToBePut';
