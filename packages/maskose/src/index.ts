ba64ec03338e9f50be308e261d4d8949654aafdc
Emanuel Freitas <efreitasn@hotmail.com>
2019-05-26 11:20:07 -0300

    Add exports to src/index.ts

--- packages/maskose/src/index.ts
+++ packages/maskose/src/index.ts
@@ -18,3 +18,15 @@ export interface MaskoseMask {
   readonly content: MaskoseMaskContentItem[];
   readonly makeContentIterator: (options: MaskoseMaskIterateContentOptions) => MaskoseMaskContentIterator;
 };
+
+export { default as mkFormat } from './fns/format';
+export { default as mkMask } from './fns/mask';
+export { default as mkMatch } from './fns/match';
+export {
+  mkCharLetter as mkCharLetter,
+  mkCharNum as mkCharNum,
+  mkCharSpecific as mkCharSpecific,
+  mkCharToBePut as mkCharToBePut,
+  mkCharRepeat as mkCharRepeat,
+  mkCharPredicateFn as mkCharPredicateFn
+} from './chars';
