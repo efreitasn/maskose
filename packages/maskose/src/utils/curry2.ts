0871f8edc6c6ef6ef0fe84880d4e65b4692089f4
Emanuel Freitas <efreitasn@hotmail.com>
2019-05-25 21:29:38 -0300

    Fix type annotations in curry2

--- packages/maskose/src/utils/curry2.ts
+++ packages/maskose/src/utils/curry2.ts
@@ -2,11 +2,11 @@
  * Curry a binary function
  * @param fn Function to be curried
  */
-export default function curry2<A1, A2, R>(fn: Function) {
-  function curried(arg1: A1, arg2: A2): R;
-  function curried(arg1: A1): (arg2: A2) => R;
+export default function curry2<T extends (...args: any[]) => any>(fn: T) {
+  function curried(arg1: Parameters<T>[0], arg2: Parameters<T>[1]): ReturnType<T>;
+  function curried(arg1: Parameters<T>[0]): (arg2: Parameters<T>[1]) => ReturnType<T>;
 
-  function curried(arg1: A1, arg2?: any): any {
+  function curried(arg1: Parameters<T>[0], arg2?: any): any {
     if (arg2 === undefined) {
       return fn.bind(null, arg1);
     }
