83969de070fb7d7c1c591e5c2c4b78fbe6e176c4
Emanuel Freitas <efreitasn@hotmail.com>
2019-05-27 12:51:18 -0300

    Remove arrayToArgs fn

--- packages/maskose/src/utils/arrayToArgs.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Higher-order function to pass items of an array as arguments to a function.
- */
-export default function arrayToArgs<TFn extends ((...args: any[]) => any)>(fn: TFn): (arr: Parameters<TFn>) => ReturnType<TFn> {
-  return function arrayToArgsWithFn(arr: Parameters<TFn>) {
-    return fn(...arr);
-  };
-}
