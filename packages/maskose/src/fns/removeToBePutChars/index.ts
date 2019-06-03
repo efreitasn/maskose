90923d8ad33cbb6e3f1a22e679ebce60ec32027b
Emanuel Freitas <efreitasn@hotmail.com>
2019-06-03 02:18:43 -0300

    Add TSDoc annotations to mkRemoveToBePutChars

--- packages/maskose/src/fns/removeToBePutChars/index.ts
+++ packages/maskose/src/fns/removeToBePutChars/index.ts
@@ -1,5 +1,9 @@
 import { toBePutCharsRegExp } from '../../mask/chars/toBePut';
 
+/**
+ * Remove all characters of the provided value that are toBePut mask
+ * characters.
+ */
 export default function mkRemoveToBePutChars(value: string): string {
   const regExp = new RegExp(
     toBePutCharsRegExp.source,
