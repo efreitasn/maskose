b71cfb032fc3644788c7c00ede750e496458d08c
Emanuel Freitas <efreitasn@hotmail.com>
2019-06-03 11:31:16 -0300

    Add a test to MaskoseInput's test file

--- packages/maskose-react/src/MaskoseInput/__tests__/index.tsx
+++ packages/maskose-react/src/MaskoseInput/__tests__/index.tsx
@@ -9,8 +9,7 @@ import {
   mkCharNum,
   mkCharToBePut,
   mkMaskBoostEndless,
-  mkMaskBoostRightToLeft,
-  MaskoseMask
+  mkMaskBoostRightToLeft
 } from 'maskose';
 
 const usCurrencyMask = mkBoostMask(
@@ -28,30 +27,17 @@ const usCurrencyMask = mkBoostMask(
   mkMaskBoostRightToLeft()
 ]);
 
-const setup = (mask: MaskoseMask) => {
+it('should call the onMaskedValueChange prop with the entered value masked', () => {
   const onMaskedValueChangeFn = jest.fn();
   const utils = render((
     <MaskoseInput
       onMaskedValueChange={onMaskedValueChangeFn}
       placeholder="Currency"
-      mask={mask}
+      mask={usCurrencyMask}
       maskedValue=""
     />
   ));
   const input = (utils.getByPlaceholderText('Currency') as HTMLInputElement);
-
-  return {
-    ...utils,
-    onMaskedValueChangeFn,
-    input
-  };
-};
-
-it('should call the onMaskedValueChange prop with the entered value masked', () => {
-  const {
-    input,
-    onMaskedValueChangeFn
-  } = setup(usCurrencyMask);
 
   fireEvent.change(
     input,
@@ -63,4 +49,29 @@ it('should call the onMaskedValueChange prop with the entered value masked', () 
   );
 
   expect(onMaskedValueChangeFn).toHaveBeenCalledWith('1,234,567.89');
+});
+
+it('should call the onChange prop with the entered value masked', () => {
+  const onChangeFn = jest.fn();
+  const utils = render((
+    <MaskoseInput
+      onChange={onChangeFn}
+      onMaskedValueChange={() => void(0)}
+      placeholder="Currency"
+      mask={usCurrencyMask}
+      maskedValue=""
+    />
+  ));
+  const input = (utils.getByPlaceholderText('Currency') as HTMLInputElement);
+
+  fireEvent.change(
+    input,
+    {
+      target: {
+        value: '123456789'
+      }
+    }
+  );
+
+  expect(onChangeFn).toHaveBeenCalledWith(expect.anything());
 });
